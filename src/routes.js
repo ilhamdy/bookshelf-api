dde066d14db725febb08176183b4dbaedbf1d14e
IlhamDy <ilham.dy18@gmail.com>
2023-01-30 02:24:32 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+/* eslint-disable object-curly-newline */
+const { addBookHandler, getAllBooksHandler, getDetailBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getDetailBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => 'Halaman tidak ditemukan',
+  },
+];
+module.exports = routes;
